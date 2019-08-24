ebb8f13f3e18c6096f9e04b0ed827ebbac56ae34
Djoe Pramono <djoepramono@gmail.com>
2019-08-25 00:08:19 +1000

    use empty binding pattern instead of any

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,24 @@ export type Validated<T> = Invalid[] | T;
 type ValidateAll<T> = (fns: Validate<T>[], t: T) => Validated<T>;
 
 type IsErrorTypeGuard<T> = (errorOrT: InvalidOr<T>) => errorOrT is Invalid;
-export const isError: IsErrorTypeGuard<any> = (errorOrT): errorOrT is Invalid => {
+// What is {} here? It's an  empty binding pattern
+// which is currently used as workaround for anonymous parameter
+// For more information see: https://github.com/Microsoft/TypeScript/issues/5586
+export const isError: IsErrorTypeGuard<{}> = (errorOrT): errorOrT is Invalid => {
     return (errorOrT as Invalid).errorMessage !== undefined;
 };
 
 type AreErrorsTypeGuard<T> = (validatedT: Validated<T>) => validatedT is Invalid[];
-export const AreErrors: AreErrorsTypeGuard<any> = (validatedT): validatedT is Invalid[] => {
+export const AreErrors: AreErrorsTypeGuard<{}> = (validatedT): validatedT is Invalid[] => {
     return (validatedT as Invalid[]).filter((e): boolean => e.errorMessage !== undefined).length > 0;
 };
 
-export const runValidations: ValidateAll<any> = (fns, input): Validated<any> => {
-    const validateResults = fns.map((fn): InvalidOr<any> => fn(input));
+export const runValidations: ValidateAll<{}> = (fns, input): Validated<{}> => {
+    const validateResults = fns.map((fn): InvalidOr<{}> => fn(input));
 
     let errors: Invalid[] = [];
     validateResults.forEach(
-        (eos: InvalidOr<any>): void => {
+        (eos: InvalidOr<{}>): void => {
             if (isError(eos)) errors.push(eos);
         }
     );
