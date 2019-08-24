6766e38b05e5ad0d7098abfafdd6ed0327abca7e
Djoe Pramono <djoepramono@gmail.com>
2019-08-24 23:40:01 +1000

    add linting

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,22 +1,22 @@
-import { Validate, runValidations, AreErrors } from "./index"
+import { Validate, runValidations, AreErrors, InvalidOr } from './index';
 
-describe("runValidations", () => {
-  interface Person { age: number; }
-  const above18: Validate<Person> = (person) => 
-    person.age > 18 ? person : { errorMessage: "not above 18" }      
+describe('runValidations', (): void => {
+    interface Person { age: number }
+    const above18: Validate<Person> = (person): InvalidOr<Person> => 
+        person.age > 18 ? person : { errorMessage: 'not above 18' };
   
-  it("returns the original object when pass validation", function() {          
-    const john = { age: 24 }
-    const result = runValidations([above18], john)
+    it('returns the original object when pass validation', (): void => {          
+        const john = { age: 24 };
+        const result = runValidations([above18], john);
       
-    expect(result).toBe(john);
-  }); 
+        expect(result).toBe(john);
+    }); 
 
-  it("returns an array of Invalid when fails validation", function() {          
-    const jane = { age: 18 }
-    const result = runValidations([above18], jane)
+    it('returns an array of Invalid when fails validation', (): void => {          
+        const jane = { age: 18 };
+        const result = runValidations([above18], jane);
       
-    expect(result).toStrictEqual([{ errorMessage: "not above 18" }]);
-    expect(AreErrors(result)).toBe(true);
-  }); 
+        expect(result).toStrictEqual([{ errorMessage: 'not above 18' }]);
+        expect(AreErrors(result)).toBe(true);
+    }); 
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 interface Invalid {
-  errorMessage: string;
+    errorMessage: string;
 }
 
 export type InvalidOr<T> = Invalid | T;
@@ -9,23 +9,23 @@ type ValidateAll<T> = (fns: Validate<T>[], t: T) => Validated<T>;
 
 type IsErrorTypeGuard<T> = (errorOrT: InvalidOr<T>) => errorOrT is Invalid;
 export const isError: IsErrorTypeGuard<any> = (errorOrT): errorOrT is Invalid => {
-  return (errorOrT as Invalid).errorMessage !== undefined;
+    return (errorOrT as Invalid).errorMessage !== undefined;
 };
 
 type AreErrorsTypeGuard<T> = (validatedT: Validated<T>) => validatedT is Invalid[];
 export const AreErrors: AreErrorsTypeGuard<any> = (validatedT): validatedT is Invalid[] => {
-  return (validatedT as Invalid[]).filter(e => e.errorMessage !== undefined).length > 0;
+    return (validatedT as Invalid[]).filter((e): boolean => e.errorMessage !== undefined).length > 0;
 };
 
 export const runValidations: ValidateAll<any> = (fns, input): Validated<any> => {
-  const x = fns.map((fn) => fn(input));
+    const validateResults = fns.map((fn): InvalidOr<any> => fn(input));
 
-  let errors: Invalid[] = [];
-  x.forEach(
-    (eos: InvalidOr<any>): void => {
-      if (isError(eos)) errors.push(eos);
-    }
-  );
+    let errors: Invalid[] = [];
+    validateResults.forEach(
+        (eos: InvalidOr<any>): void => {
+            if (isError(eos)) errors.push(eos);
+        }
+    );
 
-  return errors.length > 0 ? errors : input;
+    return errors.length > 0 ? errors : input;
 };
