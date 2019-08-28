1cd77b227ebd73b25f580d50df8b24a7c7758d16
Djoe Pramono <djoepramono@gmail.com>
2019-08-28 21:48:51 +1000

    add runAsyncValidations

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,21 @@ export const runValidations: ValidateAll = <T>(fns, input): Validated<T> => {
 
     return errors.length > 0 ? errors : input;
 };
+
+export type AsyncValidate<T> = (t: T) => Promise<InvalidOr<T>>;
+export type AsyncValidateAll = <T>(fns: AsyncValidate<T>[], t: T) => Promise<Validated<T>>;
+export const runAsyncValidations: AsyncValidateAll = async <T>(fns, input): Promise<Validated<T>> => {
+    const eventualResults = fns.map((fn): InvalidOr<{}> => {
+        return fn(input);
+    });
+
+    const validateResults: InvalidOr<{}>[] = await Promise.all(eventualResults);
+
+    let errors: Invalid[] = [];
+    validateResults.forEach(
+        (eos: Promise<InvalidOr<{}>>): void => {
+            if (eos instanceof Invalid) errors.push(eos);
+        }
+    );
+    return errors.length > 0 ? errors : input;
+};
