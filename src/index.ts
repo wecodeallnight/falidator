e1d0d02fa79068fc31934b159138d237f4278627
Djoe Pramono <djoepramono@gmail.com>
2019-09-13 00:30:07 +1000

    add index.ts back because dist needs an entry point

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+// import { runAsyncValidations } from "./asyncRun"
+export { runAsyncValidations, AsyncValidate, AsyncValidateAll } from './asyncRun';
+export { runValidations, Validate, ValidateAll } from './syncRun';
