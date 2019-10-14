1f9e40ff7fbdbf8913ff7ae4c132aeaaf1d939ca
Djoe Pramono <djoepramono@gmail.com>
2019-10-15 01:02:57 +1100

    add more types in asyncRun but somehow the result type got swapped

--- src/asyncRun.ts
+++ src/asyncRun.ts
@@ -2,19 +2,17 @@ import { Invalid, InvalidOr, Validated } from './models';
 import { isStrictInvalid } from './typeGuards';
 
 export type AsyncValidate<T> = (t: T) => Promise<InvalidOr<T>>;
-export type AsyncValidateAll = <T>(fns: AsyncValidate<T>[], t: T) => Promise<Validated<T>>;
+// export type AsyncValidateAll = <T>(fns: AsyncValidate<T>[], t: T) => Promise<Validated<T>>;
 
-type ArrayOfInvalidOr = InvalidOr<{}>[]
-
-export const runAsyncValidations: AsyncValidateAll = async <T>(fns, input): Promise<Validated<T>> => {
-    const eventualResults = fns.map((fn): InvalidOr<{}> => {
-        return fn(input);
-    });
+export const runAsyncValidations = async <T>(asyncFunctions: AsyncValidate<T>[], input: T): Promise<T | Invalid[]> => {
+    const eventualResults: Promise<InvalidOr<T>>[] = asyncFunctions.map(
+        (fn): Promise<InvalidOr<T>> => { return fn(input); }
+    );
 
     // We need to compose promises with catch because we don't want rejected Promise to bubble up
-    const wrappedPromises = eventualResults
+    const wrappedPromises: Promise<InvalidOr<T>>[] = eventualResults
         .map(
-            (eventualResult): ArrayOfInvalidOr => eventualResult
+            (eventualResult): Promise<InvalidOr<T>> => eventualResult
                 .catch(
                     // You cannot type what's catched here
                     // Because it can be Error or anything thrown by Promise.reject
@@ -29,13 +27,14 @@ export const runAsyncValidations: AsyncValidateAll = async <T>(fns, input): Prom
     // Promise.all wait for all promise to resolve OR return the first rejected promise.
     // Which is not what we want, we want to collect all errors first
     // This is why we need to compose the promises with catch above
-    const validateResults: ArrayOfInvalidOr = await Promise.all<ArrayOfInvalidOr>(wrappedPromises);
+    const validateResults: InvalidOr<T>[] = await Promise.all<InvalidOr<T>>(wrappedPromises);
 
-    const accumulateErrors = (accumulator: Invalid[], current: InvalidOr<{}>): Invalid[] => {
+    const accumulateErrors = (accumulator: Invalid[], current: InvalidOr<T>): Invalid[] => {
         if (isStrictInvalid(current)) accumulator.push(current);
         return accumulator;
     };
 
+    //the type got swapped
     const errors: Invalid[] = validateResults.reduce<Invalid[]>(accumulateErrors, []);
     return errors.length > 0 ? errors : input;
 };
