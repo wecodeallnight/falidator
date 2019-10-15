cad8020fa3eaba68794a417a77feb8e199c88af1
Djoe Pramono <djoepramono@gmail.com>
2019-10-16 00:42:58 +1100

    cleaned up minor comment in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-// import { runAsyncValidations } from "./asyncRun"
 export { runAsyncValidations, AsyncValidate, AsyncValidateAll } from './asyncRun';
 export { runValidations, Validate, ValidateAll } from './syncRun';
